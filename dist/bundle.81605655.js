16fdd1dff1dc2e72926b3cd54d76c0e9f79eddfc
SimHub <lackmann.simon@gmail.com>
2020-01-16 19:22:03 +0100

    modified style css

--- dist/bundle.81605655.js
+++ dist/bundle.81605655.js
@@ -3593,8 +3593,7 @@ function () {
   }, {
     key: "joystickControl",
     value: function joystickControl(newDirection) {
-      console.log("logMobileDir: ", newDirection);
-
+      // console.log("logMobileDir: ", newDirection);
       if (!this.directionLock) {
         this.directionLock = true;
         var h = ["left", "right", "up", "down"];
@@ -13589,7 +13588,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51178" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49967" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
