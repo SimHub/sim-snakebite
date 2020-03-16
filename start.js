bd400e25f6dee8dccf82b53af0236ca80851c813
SimHub <lackmann.simon@gmail.com>
2020-03-16 11:18:11 +0100

    modified package.json added "dev"+"build" scripts 🔖

--- start.js
+++ start.js
@@ -1,19 +1,32 @@
 import "bulma/css/bulma.css";
+import "bulma-divider/dist/css/bulma-divider.min.css";
+import "bulma-tooltip/dist/css/bulma-tooltip.min.css";
+import "typeface-press-start-2p";
+import "./node_modules/nes.css/css/nes.css";
+import $ from "jquery";
+import ClipboardJS from "./node_modules/clipboard/dist/clipboard.js";
+import "typeface-orbitron";
 import IP from "./utils";
 
 const startBtn = document.querySelector("#start");
 const createRoom = document.querySelector("#createRoom");
 const newRandomCode = document.querySelector("#newRandomCode");
+const joinRoom = document.querySelector("#joinRoom");
 const joinRoomCode = document.querySelector("#joinRoomCode");
+const btn = document.querySelector("#btn");
+const codeUrlLength = 20;
 
-createRoom.addEventListener("click", function(e) {
-  e.preventDefault();
-  let random = randCode(20);
-  newRandomCode.value = random;
-  //toBeVeryfied.text(random);
-  newRandomCode.value = random;
-  startBtn.setAttribute("href", "/" + random);
-});
+// createRoom.addEventListener("click", function(e) {
+// e.preventDefault();
+// let random = randCode(20);
+// newRandomCode.value = random;
+// //toBeVeryfied.text(random);
+// newRandomCode.value = random;
+// startBtn.setAttribute("href", "/" + random);
+// });
+
+let random = randCode(20);
+newRandomCode.value = random;
 
 function randCode(length) {
   let code = "";
@@ -21,6 +34,43 @@ function randCode(length) {
   for (let i = 0; i < length; i++)
     code += rand.charAt(Math.floor(Math.random() * rand.length));
   return code;
+}
+
+//// TABS ////
+$(".tablinks").on("click", e => {
+  let target = e.target;
+  $(".tablinks").each((key, item) => {
+    $(item).removeClass("is-active");
+    $(target)
+      .parent()
+      .addClass("is-active");
+    let dataId = $(target)
+      .parent()
+      .data("link");
+    console.log(dataId);
+    $(".tabcontent").each((k, i) => {
+      $(i).css("display", "none");
+    });
+    $(`#${dataId}`).css("display", "block");
+  });
+});
+
+//// CLIPBOARD ////
+var clipboard = new ClipboardJS(btn);
+clipboard.on("success", clip);
+clipboard.on("error", function(e) {
+  console.log(e);
+});
+function clip(e) {
+  console.log(e);
+  $("#btn")
+    .removeClass("far fa-copy")
+    .addClass("fas fa-copy");
+  $("#joinRoomCode").data("tooltip", "copied");
+  $("#copiedPopup").css("display", "block");
+  setTimeout(() => {
+    $("#copiedPopup").css("display", "none");
+  }, 2000);
 }
 
 /* ///// START GAME //// */
